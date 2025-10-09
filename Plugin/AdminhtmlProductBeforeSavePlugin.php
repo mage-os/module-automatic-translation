4580eb4dbeadea07d3bf0e4233113aaef0308a1a
dadolun95 <dadolun@gmail.com>
2025-10-09 03:51:29 +0300

    Improve plugin and service classes code structure (#36)
    * Improve plugin and service classes code structure
    
    * Formatting fixes
    
    * Formatting fixes
    
    * Formatting fixes and type safety
    
    * Formatting fixes and type safety
    
    ---------
    
    Co-authored-by: Ryan Hoerr <rhoerr@users.noreply.github.com>

--- Plugin/AdminhtmlProductBeforeSavePlugin.php
+++ Plugin/AdminhtmlProductBeforeSavePlugin.php
@@ -3,14 +3,14 @@
 namespace MageOS\AutomaticTranslation\Plugin;
 
 use Exception;
-use Magento\Catalog\Api\Data\ProductInterface;
-use Magento\Catalog\Controller\Adminhtml\Product\Save;
 use MageOS\AutomaticTranslation\Helper\ModuleConfig;
 use MageOS\AutomaticTranslation\Helper\Service;
 use MageOS\AutomaticTranslation\Model\Config\Source\TextAttributes;
 use MageOS\AutomaticTranslation\Model\Translator;
-use Magento\Framework\Message\ManagerInterface;
+use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\Catalog\Controller\Adminhtml\Product\Save;
 use Magento\Catalog\Model\ResourceModel\Product\Gallery;
+use Magento\Framework\Message\ManagerInterface;
 use Psr\Log\LoggerInterface as Logger;
 
 /**
@@ -58,12 +58,12 @@ class AdminhtmlProductBeforeSavePlugin
      * @param Logger $logger
      */
     public function __construct(
-        ModuleConfig $moduleConfig,
-        Service $serviceHelper,
-        Translator $translator,
-        Gallery $gallery,
+        ModuleConfig     $moduleConfig,
+        Service          $serviceHelper,
+        Translator       $translator,
+        Gallery          $gallery,
         ManagerInterface $messageManager,
-        Logger $logger
+        Logger           $logger
     ) {
         $this->moduleConfig = $moduleConfig;
         $this->serviceHelper = $serviceHelper;
@@ -81,79 +81,63 @@ class AdminhtmlProductBeforeSavePlugin
     {
         try {
             $request = $subject->getRequest();
+
+            if ($request->getParam('translate') !== "true") {
+                return null;
+            }
+
             $requestPostValue = $request->getPostValue();
+            $storeId = $request->getParam('store', 0);
+            $sourceLanguage = $this->moduleConfig->getSourceLanguage();
+            $destinationLanguage = $this->moduleConfig->getDestinationLanguage($storeId);
 
-            if ($request->getParam('translate') === "true") {
-                $storeId = $request->getParam('store', 0);
-                $sourceLanguage = $this->moduleConfig->getSourceLanguage();
-                $destinationLanguage = $this->moduleConfig->getDestinationLanguage($storeId);
+            if ($sourceLanguage === $destinationLanguage) {
+                return null;
+            }
 
-                if ($sourceLanguage !== $destinationLanguage) {
-                    $txtAttributesToTranslate = $this->moduleConfig->getProductTxtAttributeToTranslate($storeId);
-
-                    foreach ($txtAttributesToTranslate as $attributeCode) {
-                        if ($attributeCode === TextAttributes::GALLERY_ALT_ATTRIBUTE_CODE &&
-                            isset($requestPostValue["product"]["media_gallery"]["images"])) {
-                            $mediaGalleryImages = $requestPostValue["product"]["media_gallery"]["images"];
-                            foreach ($mediaGalleryImages as $index => $mediaImage) {
-                                $mediaGalleryImages[$index]["label"] = $this->translator
-                                    ->translate($mediaImage["label"], $destinationLanguage, $sourceLanguage);
-                            }
-                            $requestPostValue["product"]["media_gallery"]["images"] = $mediaGalleryImages;
-
-                        } else {
-                            if (!empty($requestPostValue["product"][$attributeCode]) &&
-                                is_string($requestPostValue["product"][$attributeCode])
-                            ) {
-                                $originalValue = $requestPostValue["product"][$attributeCode];
-
-                                $parsedContent = $this->serviceHelper
-                                    ->parsePageBuilderHtmlBox($requestPostValue["product"][$attributeCode]);
-
-                                if (is_string($parsedContent)) {
-                                    $requestPostValue["product"][$attributeCode] = $this->translator->translate(
-                                        $parsedContent,
-                                        $destinationLanguage
-                                    );
-                                } else {
-                                    $requestPostValue["product"][$attributeCode] = html_entity_decode(
-                                        htmlspecialchars_decode($requestPostValue["product"][$attributeCode])
-                                    );
-                                    foreach ($parsedContent as $parsedString) {
-                                        $parsedString["translation"] = $this->translator->translate(
-                                            $parsedString["source"],
-                                            $destinationLanguage
-                                        );
-
-                                        $requestPostValue["product"][$attributeCode] = str_replace(
-                                            $parsedString["source"],
-                                            $parsedString["translation"],
-                                            $requestPostValue["product"][$attributeCode]
-                                        );
-                                    }
-
-                                    $requestPostValue["product"][$attributeCode] = $this->serviceHelper
-                                        ->encodePageBuilderHtmlBox($requestPostValue["product"][$attributeCode]);
-                                }
-
-                                if ($attributeCode === 'url_key') {
-                                    $requestPostValue["product"][$attributeCode] = strtolower(
-                                        preg_replace('#[^0-9a-z]+#i', '-', $requestPostValue["product"][$attributeCode])
-                                    );
-                                }
-
-                                $translatedValue = $requestPostValue["product"][$attributeCode];
-
-                                if ($originalValue !== $translatedValue) {
-                                    $requestPostValue['use_default'][$attributeCode] = '0';
-                                }
-                            }
-                        }
+            $txtAttributesToTranslate = $this->moduleConfig->getProductTxtAttributeToTranslate($storeId);
+            foreach ($txtAttributesToTranslate as $attributeCode) {
+                if ($attributeCode === TextAttributes::GALLERY_ALT_ATTRIBUTE_CODE &&
+                    isset($requestPostValue["product"]["media_gallery"]["images"])) {
+                    $mediaGalleryImages = $requestPostValue["product"]["media_gallery"]["images"];
+                    foreach ($mediaGalleryImages as $index => $mediaImage) {
+                        $mediaGalleryImages[$index]["label"] = $this->translator
+                            ->translate((string)$mediaImage["label"], $destinationLanguage, $sourceLanguage);
+                    }
+                    $requestPostValue["product"]["media_gallery"]["images"] = $mediaGalleryImages;
+                } else {
+                    if (empty($requestPostValue["product"][$attributeCode]) ||
+                        !is_string($requestPostValue["product"][$attributeCode])
+                    ) {
+                        continue;
                     }
 
-                    $request->setPostValue($requestPostValue);
+                    $originalValue = $requestPostValue["product"][$attributeCode];
+
+                    $parsedContent = $this->serviceHelper
+                        ->parsePageBuilderHtmlBox($requestPostValue["product"][$attributeCode]);
+
+                    $requestPostValue["product"][$attributeCode] = $this->translateParsedContent(
+                        $parsedContent,
+                        $requestPostValue["product"][$attributeCode],
+                        $destinationLanguage
+                    );
+
+                    if ($attributeCode === 'url_key') {
+                        $requestPostValue["product"][$attributeCode] = strtolower(
+                            preg_replace('#[^0-9a-z]+#i', '-', $requestPostValue["product"][$attributeCode])
+                        );
+                    }
+
+                    $translatedValue = $requestPostValue["product"][$attributeCode];
+
+                    if ($originalValue !== $translatedValue) {
+                        $requestPostValue['use_default'][$attributeCode] = '0';
+                    }
                 }
             }
+
+            $request->setPostValue($requestPostValue);
         } catch (Exception $e) {
             $this->logger->debug(__("An error translating product attributes: %s", $e->getMessage()));
             $this->messageManager->addErrorMessage(
@@ -161,5 +145,38 @@ class AdminhtmlProductBeforeSavePlugin
             );
         }
         return null;
+    }
+
+    /**
+     * @param mixed $parsedContent
+     * @param string $requestPostValue
+     * @param string $destinationLanguage
+     * @return mixed|string
+     */
+    protected function translateParsedContent($parsedContent, string $requestPostValue, string $destinationLanguage)
+    {
+        if (is_string($parsedContent)) {
+            return $this->translator->translate(
+                $parsedContent,
+                $destinationLanguage
+            );
+        }
+        
+        $requestPostValue = html_entity_decode(
+            htmlspecialchars_decode($requestPostValue)
+        );
+        foreach ($parsedContent as $parsedString) {
+            $parsedString["translation"] = $this->translator->translate(
+                (string)$parsedString["source"],
+                $destinationLanguage
+            );
+
+            $requestPostValue = str_replace(
+                $parsedString["source"],
+                $parsedString["translation"],
+                $requestPostValue
+            );
+        }
+        return $this->serviceHelper->encodePageBuilderHtmlBox($requestPostValue);
     }
 }
--- Service/ProductTranslator.php
+++ Service/ProductTranslator.php
@@ -2,21 +2,21 @@
 
 namespace MageOS\AutomaticTranslation\Service;
 
-use Magento\Framework\Exception\LocalizedException;
+use Exception;
+use MageOS\AutomaticTranslation\Api\AttributeProviderInterface;
 use MageOS\AutomaticTranslation\Api\ProductTranslatorInterface;
+use MageOS\AutomaticTranslation\Api\TranslatorInterface;
 use MageOS\AutomaticTranslation\Helper\ModuleConfig;
 use MageOS\AutomaticTranslation\Helper\Service as ServiceHelper;
-use MageOS\AutomaticTranslation\Api\AttributeProviderInterface;
-use Magento\Framework\DataObject;
-use Magento\Catalog\Api\Data\ProductInterface;
-use MageOS\AutomaticTranslation\Api\TranslatorInterface;
-use Magento\Catalog\Model\ResourceModel\Product as ProductResource;
-use Psr\Log\LoggerInterface as Logger;
-use Magento\Catalog\Model\ResourceModel\Product\Gallery;
 use MageOS\AutomaticTranslation\Model\Config\Source\TextAttributes;
-use Magento\Store\Model\StoreManagerInterface;
+use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\Catalog\Model\ResourceModel\Product as ProductResource;
+use Magento\Catalog\Model\ResourceModel\Product\Gallery;
 use Magento\CatalogUrlRewrite\Model\Products\AppendUrlRewritesToProducts;
-use Exception;
+use Magento\Framework\DataObject;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Store\Model\StoreManagerInterface;
+use Psr\Log\LoggerInterface as Logger;
 
 /**
  * Class ProductTranslator
@@ -106,102 +106,31 @@ class ProductTranslator implements ProductTranslatorInterface
         foreach ($attributesToTranslate as $attributeCode) {
             if ($attributeCode === TextAttributes::GALLERY_ALT_ATTRIBUTE_CODE) {
                 $product->setStoreId($storeId);
-                $gallery = $this->gallery->loadProductGalleryByAttributeId(
-                    $product,
-                    (int)$this->productResource
-                        ->getAttribute(ProductInterface::MEDIA_GALLERY)->getAttributeId()
-                );
-
-                foreach ($gallery as $mediaImage) {
-                    $altTextRows = $this->gallery->loadDataFromTableByValueId(
-                        $this->gallery::GALLERY_VALUE_TABLE,
-                        [$mediaImage["value_id"]]
-                    );
-                    $textToTranslate = null;
-                    $translationPosition = 0;
-                    $translationDisabled = 0;
-
-                    foreach ($altTextRows as $altTextRow) {
-                        if ($altTextRow["store_id"] === "0") {
-                            $textToTranslate = $altTextRow["label"];
-                        }
-                        if ($altTextRow["store_id"] === (string)$storeId) {
-                            $translationPosition = $altTextRow["position"];
-                            $textToTranslate = $altTextRow["label"];
-                            $translationDisabled = $altTextRow["disabled"];
-                            $this->gallery->deleteGalleryValueInStore(
-                                $mediaImage["value_id"],
-                                $product->getId(),
-                                $storeId
-                            );
-                            break;
-                        }
-                    }
-
-                    if ($textToTranslate) {
-                        $translatedText = $this->translator->translate(
-                            $textToTranslate,
-                            $targetLanguage,
-                            $sourceLanguage
-                        );
-                        $this->gallery->insertGalleryValueInStore([
-                            "value_id" => $mediaImage["value_id"],
-                            "store_id" => $storeId,
-                            "entity_id" => $product->getId(),
-                            "label" => $translatedText,
-                            "position" => $translationPosition,
-                            "disabled" => $translationDisabled
-                        ]);
-                    }
-                }
+                $this->translateGalleryAlternativeTexts($product, $storeId, $targetLanguage, $sourceLanguage);
             } else {
                 $textToTranslate = $product->getData($attributeCode);
-                if (!empty($textToTranslate)) {
-                    try {
-                        $parsedContent = $this->serviceHelper->parsePageBuilderHtmlBox($textToTranslate);
+                if (!is_string($textToTranslate) || empty($textToTranslate)) {
+                    continue;
+                }
+                try {
+                    $parsedContent = $this->serviceHelper->parsePageBuilderHtmlBox($textToTranslate);
+                    $textTranslated = $this->translateParsedContent($parsedContent, $textToTranslate, $targetLanguage, $sourceLanguage);
 
-                        if (is_string($parsedContent)) {
-                            $textTranslated = $this->translator->translate(
-                                $textToTranslate,
-                                $targetLanguage,
-                                $sourceLanguage
-                            );
-                        } else {
-                            $textToTranslate = html_entity_decode(htmlspecialchars_decode($textToTranslate));
-                            $textTranslated = $textToTranslate;
+                    if ($textToTranslate !== $textTranslated) {
+                        $product->setData($attributeCode, $textTranslated);
+                        $this->productResource->saveAttribute($product, $attributeCode);
 
-                            foreach ($parsedContent as $parsedString) {
-                                $parsedString["translation"] = $this->translator->translate(
-                                    $parsedString["source"],
-                                    $targetLanguage
-                                );
-
-                                $textTranslated = str_replace(
-                                    $parsedString["source"],
-                                    $parsedString["translation"],
-                                    $textTranslated
-                                );
-                            }
-
-                            $textTranslated = $this->serviceHelper->encodePageBuilderHtmlBox($textTranslated);
+                        if ($this->moduleConfig->enableUrlRewrite($storeId) && $attributeCode === 'url_key') {
+                            $this->appendRewrites->execute([$product], [$storeId]);
                         }
-
-                        if ($textToTranslate != $textTranslated) {
-                            $product->setData($attributeCode, $textTranslated);
-                            $this->productResource->saveAttribute($product, $attributeCode);
-
-                            if ($this->moduleConfig->enableUrlRewrite($storeId) && $attributeCode === 'url_key') {
-                                $this->appendRewrites->execute([$product], [$storeId]);
-                            }
-                        }
-                    } catch (Exception $e) {
-                        $this->logger->debug('Error when translating the product');
-                        $this->logger->debug('Product sku: ' . $product->getSku());
-                        $this->logger->debug('Store: ' . $storeName . '(id ' . $storeId . ')');
-                        $this->logger->debug('Attribute: ' . $attributeCode);
-                        $this->logger->debug($e->getMessage());
-                        $this->logger->debug('-------------------------');
                     }
+                } catch (Exception $e) {
+                    $this->logger->debug('Error when translating the product');
+                    $this->logger->debug('Product sku: ' . $product->getSku());
+                    $this->logger->debug('Store: ' . $storeName . '(id ' . $storeId . ')');
+                    $this->logger->debug('Attribute: ' . $attributeCode);
+                    $this->logger->debug($e->getMessage());
+                    $this->logger->debug('-------------------------');
                 }
             }
         }
@@ -227,5 +156,102 @@ class ProductTranslator implements ProductTranslatorInterface
             $this->logger->debug($e->getMessage());
             $this->logger->debug('-------------------------');
         }
+    }
+
+    /**
+     * @param ProductInterface $product
+     * @param int|string $storeId
+     * @param string $targetLanguage
+     * @param string $sourceLanguage
+     * @return void
+     * @throws LocalizedException
+     */
+    protected function translateGalleryAlternativeTexts($product, $storeId, string $targetLanguage, string $sourceLanguage): void
+    {
+        $gallery = $this->gallery->loadProductGalleryByAttributeId(
+            $product,
+            (int)$this->productResource
+                ->getAttribute(ProductInterface::MEDIA_GALLERY)->getAttributeId()
+        );
+
+        foreach ($gallery as $mediaImage) {
+            $altTextRows = $this->gallery->loadDataFromTableByValueId(
+                $this->gallery::GALLERY_VALUE_TABLE,
+                [$mediaImage["value_id"]]
+            );
+            $textToTranslate = null;
+            $translationPosition = 0;
+            $translationDisabled = 0;
+
+            foreach ($altTextRows as $altTextRow) {
+                if ($altTextRow["store_id"] === "0") {
+                    $textToTranslate = $altTextRow["label"];
+                }
+                if ($altTextRow["store_id"] === (string)$storeId) {
+                    $translationPosition = $altTextRow["position"];
+                    $textToTranslate = $altTextRow["label"];
+                    $translationDisabled = $altTextRow["disabled"];
+                    $this->gallery->deleteGalleryValueInStore(
+                        $mediaImage["value_id"],
+                        $product->getId(),
+                        $storeId
+                    );
+                    break;
+                }
+            }
+
+            if (is_string($textToTranslate) && !empty($textToTranslate)) {
+                $translatedText = $this->translator->translate(
+                    $textToTranslate,
+                    $targetLanguage,
+                    $sourceLanguage
+                );
+                $this->gallery->insertGalleryValueInStore([
+                    "value_id" => $mediaImage["value_id"],
+                    "store_id" => $storeId,
+                    "entity_id" => $product->getId(),
+                    "label" => $translatedText,
+                    "position" => $translationPosition,
+                    "disabled" => $translationDisabled
+                ]);
+            }
+        }
+    }
+
+    /**
+     * @param string $parsedContent
+     * @param string $textToTranslate
+     * @param string $targetLanguage
+     * @param string $sourceLanguage
+     * @return mixed|string
+     * @throws Exception
+     */
+    protected function translateParsedContent(string $parsedContent, string $textToTranslate, string $targetLanguage, string $sourceLanguage)
+    {
+        if (is_string($parsedContent)) {
+            return $this->translator->translate(
+                $textToTranslate,
+                $targetLanguage,
+                $sourceLanguage
+            );
+        }
+        
+        $textToTranslate = html_entity_decode(htmlspecialchars_decode($textToTranslate));
+        $textTranslated = $textToTranslate;
+
+        foreach ($parsedContent as $parsedString) {
+            $parsedString["translation"] = $this->translator->translate(
+                $parsedString["source"],
+                $targetLanguage
+            );
+
+            $textTranslated = str_replace(
+                $parsedString["source"],
+                $parsedString["translation"],
+                $textTranslated
+            );
+        }
+
+        return $this->serviceHelper->encodePageBuilderHtmlBox($textTranslated);
     }
 }
